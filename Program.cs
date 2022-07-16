0b917b0b210d711332d209d4965459e118c8392b
Afamsv <afamsv1234@gmail.com>
2022-07-16 14:46:42 +0100

    Printing out numbers with different characters

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ctr;
+            char ch;
+
+            Console.WriteLine("\nNumber Value\n");
+            
+            for (ctr = 63; ctr <= 94; ctr = ctr + 1)
+            {
+                ch = (char)ctr;
+                Console.WriteLine("{0} is {1}", ctr, ch);
+            }
+
+        }
+    }
+}
